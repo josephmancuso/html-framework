a373a8faaec6f96488fe7cb81bb591839c02b07d
Joseph Mancuso <idmann509@gmail.com>
2017-11-24 16:19:15 -0500

    added text, password, and submit to form module

--- js/parser.js
+++ js/parser.js
@@ -53,3 +53,64 @@ $('menu').each(function(){
     </div><!-- /.container-fluid -->
     </nav>`);
 })
+
+$('form').each(function(){
+
+    let textInputs = '';
+
+    $(this).children('text').each(function(){
+        $(this).replaceWith(`<div class="form-group">
+            <label for="text">${$(this).html()}</label>
+            <input type="text" class="form-control" id="text" placeholder="${$(this).attr('placeholder') ? $(this).attr('placeholder') : ''}">
+        </div>`)
+        console.log('replaced')
+    })
+
+    $(this).children('password').each(function(){
+        $(this).replaceWith(`<div class="form-group">
+            <label for="password">${$(this).html() ? $(this).html() : 'Password'}</label>
+            <input type="password" class="form-control" id="password">
+        </div>`)
+        console.log('password')
+    })
+
+    $(this).children('submit').each(function(){
+        $(this).replaceWith(`<div class="form-group">
+            <button type="submit" class="btn ${$(this).attr('class') ? $(this).attr('class') : 'btn-primary'}">
+                ${$(this).html() ? $(this).html() : 'Submit'}
+            </button>
+        </div>`)
+        console.log('password')
+    })
+
+
+
+
+
+
+
+
+    // $(this).replaceWith(
+    //     `<form>
+    //         <div class="form-group">
+    //             <label for="exampleInputEmail1">Email address</label>
+    //             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
+    //         </div>
+    //         <div class="form-group">
+    //             <label for="exampleInputPassword1">Password</label>
+    //             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    //         </div>
+    //         <div class="form-group">
+    //             <label for="exampleInputFile">File input</label>
+    //             <input type="file" id="exampleInputFile">
+    //             <p class="help-block">Example block-level help text here.</p>
+    //         </div>
+    //         <div class="checkbox">
+    //             <label>
+    //             <input type="checkbox"> Check me out
+    //             </label>
+    //         </div>
+    //         <button type="submit" class="btn btn-default">Submit</button>
+    //         </form>`
+    // );
+})
